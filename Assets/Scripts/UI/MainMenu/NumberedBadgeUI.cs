426724cb11f71534ab54847a4ccd2317a306a734
MastroCaleb <86205799+MastroCaleb@users.noreply.github.com>
2026-08-22 17:21:58 +0200

    bug fixes

--- Assets/Scripts/UI/MainMenu/NumberedBadgeUI.cs
+++ Assets/Scripts/UI/MainMenu/NumberedBadgeUI.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,13 +11,15 @@ public class NumberedBadgeUI : MonoBehaviour
     public Image badge;
     public Sprite smallBadge;
     public Sprite bigBadge;
+    
+    public SummonBannerManager manager;
 
     public void UpdateBadge()
     {
         int number = 0;
         if(type == BadgeType.Summon)
         {
-            number = PlayerData.gems / 1;
+            number = GetHighestAmountOfSummons();
         }
         else if(type == BadgeType.Presents)
         {
@@ -35,6 +39,29 @@ public class NumberedBadgeUI : MonoBehaviour
         numbers.SetText(text);
         badge.sprite = number > 99 ? bigBadge : smallBadge;
         badge.SetNativeSize();
+    }
+
+    public int GetHighestAmountOfSummons(){
+        List<int> amounts = new List<int>();
+        foreach(var s in manager.summonBanners){
+            int amount = 0;
+            switch (s.costType)
+            {
+                case CostType.Gems:
+                    amount = PlayerData.gems / s.cost;
+                    break;
+                case CostType.Zel:
+                    amount = PlayerData.zel / s.cost;
+                    break;
+                case CostType.Karma:
+                    amount = PlayerData.karma / s.cost;
+                    break;
+            }
+
+            amounts.Add(amount);
+        }
+
+        return amounts.Max();
     }
 }
 public enum BadgeType
